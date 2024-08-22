d15a8e17a83e2b3745b509f9d08d03a5d829c3d8
itzkbp <hi@kailashpoudel.com.np>
2024-08-22 10:52:10 +0545

    complete parsing single record insert statementtt

--- internal/sql/insertinto.go
+++ internal/sql/insertinto.go
@@ -18,15 +18,53 @@ func (t *_InsertStmt) Execute() *_Result {
 
 func (p *_Parser) parseColumnNames() []string {
 	var columns []string
-	// var column string
+	var column string
 
 	if p.token.Type == TK_LPAREN {
+		p.expect(TK_LPAREN, "(")
+		p.nextToken() // column name
 
+		for p.token.Type != TK_RPAREN {
+			p.expect(TK_IDENTIFIER, "Column Name")
+			column = p.token.Value
+			columns = append(columns, column)
+			p.nextToken() // comma or rparen
+
+			if p.token.Type == TK_COMMA {
+				p.expect(TK_COMMA, ",")
+				p.nextToken() // next column
+			}
+		}
+
+		p.expect(TK_RPAREN, ")")
+		p.nextToken()
 	} else {
 		// fill columns array with the columns from table definition
 	}
-
 	return columns
+}
+
+func (p *_Parser) parseColumnDatas() []string {
+	var datas []string
+	var data string
+
+	p.expect(TK_LPAREN, "(")
+	p.nextToken() // datas (data, ...)
+	for p.token.Type != TK_RPAREN {
+		p.expect(TK_VAL_LITERAL, "Literal Value")
+		data = p.token.Value
+		datas = append(datas, data)
+		p.nextToken()
+
+		if p.token.Type == TK_COMMA {
+			p.expect(TK_COMMA, ",")
+			p.nextToken() // next column
+		}
+	}
+
+	p.expect(TK_RPAREN, ")")
+	p.nextToken()
+	return datas
 }
 
 func parseInsertInto(p *_Parser) _SQLQuery {
@@ -38,6 +76,9 @@ func parseInsertInto(p *_Parser) _SQLQuery {
 	p.nextToken() // Columns (column1, column2, ...)
 	stmt.columns = p.parseColumnNames()
 	p.expect(TK_KW_VALUES, "VALUES")
+	p.nextToken() // lparen
+	stmt.data = p.parseColumnDatas()
+	p.expect(TK_SEMICOLON, ";")
 
 	return &stmt
 }
